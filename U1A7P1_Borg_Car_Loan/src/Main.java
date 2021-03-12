bb1a2d3d62176de63405ffa23a7a456572ac27f8
mat4539 <mborg0020@gmail.com>
2021-03-12 09:50:47 -0500

    Built GUI

--- U1A7P1_Borg_Car_Loan/src/Main.java
+++ U1A7P1_Borg_Car_Loan/src/Main.java
@@ -26,21 +26,204 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        title = new javax.swing.JLabel();
+        author = new javax.swing.JLabel();
+        amountTitle = new javax.swing.JLabel();
+        annualIntrestRateTitle = new javax.swing.JLabel();
+        timePeriodTitle = new javax.swing.JLabel();
+        monthlyPaymentTitle = new javax.swing.JLabel();
+        totalInterestPaidTitle = new javax.swing.JLabel();
+        totalPaybackTitle = new javax.swing.JLabel();
+        calculate = new javax.swing.JButton();
+        clear = new javax.swing.JButton();
+        inputLoanAmount = new javax.swing.JTextField();
+        inputAnnualIntrestRate = new javax.swing.JTextField();
+        inputTimePeriod = new javax.swing.JTextField();
+        outputMonthlyPayment = new javax.swing.JTextField();
+        outputTotalIntrestPaid = new javax.swing.JTextField();
+        outputTotalPayback = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        title.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        title.setForeground(new java.awt.Color(10, 255, 50));
+        title.setText("Car Loan");
+
+        author.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        author.setText("By: Mathew Borg");
+
+        amountTitle.setText("Loan Amount ($)");
+
+        annualIntrestRateTitle.setText("Annual Intrest Rate (%)");
+
+        timePeriodTitle.setText("Time Period (Years)");
+
+        monthlyPaymentTitle.setText("Monthly Payment");
+
+        totalInterestPaidTitle.setText("Total Interest Paid");
+
+        totalPaybackTitle.setText("Total Payback");
+
+        calculate.setText("Calculate");
+        calculate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                calculateActionPerformed(evt);
+            }
+        });
+
+        clear.setText("Clear");
+        clear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clearActionPerformed(evt);
+            }
+        });
+
+        inputLoanAmount.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inputLoanAmountActionPerformed(evt);
+            }
+        });
+
+        inputAnnualIntrestRate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inputAnnualIntrestRateActionPerformed(evt);
+            }
+        });
+
+        inputTimePeriod.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inputTimePeriodActionPerformed(evt);
+            }
+        });
+
+        outputMonthlyPayment.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                outputMonthlyPaymentActionPerformed(evt);
+            }
+        });
+
+        outputTotalIntrestPaid.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                outputTotalIntrestPaidActionPerformed(evt);
+            }
+        });
+
+        outputTotalPayback.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                outputTotalPaybackActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(43, 43, 43)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(amountTitle)
+                    .addComponent(annualIntrestRateTitle)
+                    .addComponent(timePeriodTitle)
+                    .addComponent(monthlyPaymentTitle)
+                    .addComponent(totalInterestPaidTitle)
+                    .addComponent(totalPaybackTitle))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(calculate))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(inputLoanAmount)
+                        .addComponent(inputAnnualIntrestRate)
+                        .addComponent(inputTimePeriod)
+                        .addComponent(outputMonthlyPayment)
+                        .addComponent(outputTotalIntrestPaid)
+                        .addComponent(outputTotalPayback, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(60, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(author)
+                            .addComponent(title))
+                        .addGap(121, 121, 121))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(clear)
+                        .addGap(167, 167, 167))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(title)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(author)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(inputLoanAmount, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(amountTitle))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(inputAnnualIntrestRate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(annualIntrestRateTitle))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(inputTimePeriod, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(timePeriodTitle))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
+                .addComponent(calculate)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(monthlyPaymentTitle)
+                    .addComponent(outputMonthlyPayment, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(totalInterestPaidTitle)
+                    .addComponent(outputTotalIntrestPaid, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(outputTotalPayback, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(totalPaybackTitle))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(clear)
+                .addGap(18, 18, 18))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void calculateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_calculateActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_calculateActionPerformed
+
+    private void inputAnnualIntrestRateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputAnnualIntrestRateActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inputAnnualIntrestRateActionPerformed
+
+    private void inputLoanAmountActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputLoanAmountActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inputLoanAmountActionPerformed
+
+    private void inputTimePeriodActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputTimePeriodActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_inputTimePeriodActionPerformed
+
+    private void outputMonthlyPaymentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_outputMonthlyPaymentActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_outputMonthlyPaymentActionPerformed
+
+    private void outputTotalIntrestPaidActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_outputTotalIntrestPaidActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_outputTotalIntrestPaidActionPerformed
+
+    private void outputTotalPaybackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_outputTotalPaybackActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_outputTotalPaybackActionPerformed
+
+    private void clearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_clearActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +261,21 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel amountTitle;
+    private javax.swing.JLabel annualIntrestRateTitle;
+    private javax.swing.JLabel author;
+    private javax.swing.JButton calculate;
+    private javax.swing.JButton clear;
+    private javax.swing.JTextField inputAnnualIntrestRate;
+    private javax.swing.JTextField inputLoanAmount;
+    private javax.swing.JTextField inputTimePeriod;
+    private javax.swing.JLabel monthlyPaymentTitle;
+    private javax.swing.JTextField outputMonthlyPayment;
+    private javax.swing.JTextField outputTotalIntrestPaid;
+    private javax.swing.JTextField outputTotalPayback;
+    private javax.swing.JLabel timePeriodTitle;
+    private javax.swing.JLabel title;
+    private javax.swing.JLabel totalInterestPaidTitle;
+    private javax.swing.JLabel totalPaybackTitle;
     // End of variables declaration//GEN-END:variables
 }
