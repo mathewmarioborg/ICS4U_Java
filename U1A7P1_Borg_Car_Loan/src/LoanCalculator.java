3dcd6be445e3e8ac14d7e86d9ff0f9c2c8e250f9
mat4539 <mborg0020@gmail.com>
2021-03-12 17:38:23 -0500

    Fixed Calculations and Rounded answers to 2 decimal places

--- U1A7P1_Borg_Car_Loan/src/LoanCalculator.java
+++ U1A7P1_Borg_Car_Loan/src/LoanCalculator.java
@@ -14,12 +14,15 @@ public class LoanCalculator {
     Return: double 
     */
     public static double monthlyPayment(double p, double i, double t){
+        i /= 100.0; //converts to a decimal
         double monthlyRate = i/12; //calculates monthly rate
         double payments = t*12; //calculates payment periods
         
-        double monthlyPayment = (p*monthlyRate)/Math.pow(1-(1+monthlyRate), -1*payments); //calculates monthly payments (monthly payment = P * monthly rate / 1 - (1 + monthly rate)^ (-1* payments)
+        double monthlyPayment = (p * monthlyRate) / (1-Math.pow((1+monthlyRate), -payments)); //calculates monthly payments (monthly payment = P * monthly rate / 1 - (1 + monthly rate)^ (-1* payments)
         
-        return monthlyPayment; //returns the value
+        double monthlyPaymentRounded = Math.round(monthlyPayment*100.0)/100.0; // rounds numbers to 2 decimal places
+        
+        return monthlyPaymentRounded; //returns the value
     }
     /*
     Function: totalPayback
@@ -35,7 +38,9 @@ public class LoanCalculator {
         
         double totalPayback = monthlyPayment * payments; //calculates total payback total (payback = monthly payment * payments)
         
-        return totalPayback; //returns totalPayback 
+        double totalPaybackRounded = Math.round(totalPayback*100.0)/100.0; // rounds numbers to 2 decimal places
+        
+        return totalPaybackRounded; //returns totalPayback 
     }
     /*
     Function: totalIntrestPayed
@@ -50,6 +55,8 @@ public class LoanCalculator {
         
         double totalIntrestPayed = totalPayback - p; // calculates totalIntrestPayed (payments = T * 12)
         
-        return totalIntrestPayed; // retures totalIntrestPayed
+        double totalIntrestPayedRounded = Math.round(totalIntrestPayed*100.0)/100.0; // rounds numbers to 2 decimal places
+
+        return totalIntrestPayedRounded; // retures totalIntrestPayed
     }
 }
