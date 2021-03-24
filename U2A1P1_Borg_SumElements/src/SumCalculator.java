6baffca2ea792d94f79ba4e722b46a32d1349b1e
mat4539 <mborg0020@gmail.com>
2021-03-24 11:57:19 -0400

    Created working sumCalculator and sumEvenCalculator

--- U2A1P1_Borg_SumElements/src/SumCalculator.java
+++ U2A1P1_Borg_SumElements/src/SumCalculator.java
@@ -3,11 +3,22 @@ import java.util.*;
 public class SumCalculator {
     public int sum; //creats public ints for sum and product
 
-    public static int sumCalculator(List<Integer> list){ ////creats a method and int to use inside of it
+    public static double sumCalculator(ArrayList <Integer> inputSumNum){ //creats a int method with an Interger ArrayList input
         int sum = 0;
-        for (int i: list){
+        for (int i: inputSumNum){
             sum += i;
         }
         return sum;
+    }
+    
+    public static double sumEvenCalculator(ArrayList <Integer> inputSumEven){ //creats a int method with an Interger ArrayList input
+        ArrayList <Integer> sumEven = new ArrayList(); //creats a array to store the even numbers in
+        for (int i=0; i < inputSumEven.size(); i++) { //creats a for loop of the array input
+            if(inputSumEven.get(i)%2 == 0){ //finds all the even numbers by result of i%2 (if it = 0 the number is even)
+                sumEven.add(inputSumEven.get(i));
+            }
+        }
+        double sum = sumCalculator(sumEven);//send the sumEven string to sumCalculator function to calculate sun
+        return sum; //returns the sum var
     }
 }
