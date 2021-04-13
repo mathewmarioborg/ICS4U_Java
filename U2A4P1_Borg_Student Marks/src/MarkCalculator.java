8fb7b58604aa9494ad451bb9068f35e41f309f92
mat4539 <mborg0020@gmail.com>
2021-04-13 19:02:11 -0400

    Added working name finder

--- U2A4P1_Borg_Student Marks/src/MarkCalculator.java
+++ U2A4P1_Borg_Student Marks/src/MarkCalculator.java
@@ -12,17 +12,29 @@ import java.util.ArrayList;
  * @author Mathew Borg
  */
 public class MarkCalculator {
-    
-    public static double studentAverage(ArrayList<ArrayList<String>> studentData, String firstName, String LastName){
-       double theStudentAverage = 0;
-       int getPosition = studentData.indexOf(LastName);
-       
-       return theStudentAverage;
-    }
-    
-    public static double courseAverage(ArrayList<ArrayList<String>> studentData){
-        double theCourseAverage = 0;
+
+    public static double studentAverage(ArrayList<ArrayList<String>> studentData, String firstName, String LastName) {
+        double theStudentAverage = 0;
+        for (int i = 0; i < studentData.size(); i++) {
+            for (int j = 0; j < studentData.get(i).size(); j++) {
+                if(studentData.get(i).get(j).equals(firstName)){
+                   System.out.println("i=" + i + " j=" + j); 
+                   System.out.println(studentData.get(i).get(0)); 
+                   System.out.println(studentData.get(i).get(1));
+                   System.out.println(studentData.get(i).get(2));
+                   System.out.println(studentData.get(i).get(3));
+                   System.out.println(studentData.get(i).get(4));
+                   System.out.println(studentData.get(i).get(5));
+                }
+            }
+        }
         
+        return theStudentAverage;
+    }
+
+    public static double courseAverage(ArrayList<ArrayList<String>> studentData) {
+        double theCourseAverage = 0;
+
         return theCourseAverage;
     }
 }
