55fcefdc11321ffab84d7d416c5b0b87aa2ecb7e
mat4539 <mborg0020@gmail.com>
2021-04-13 10:57:14 -0400

    Created Methods

--- U2A4P1_Borg_Student Marks/src/MarkCalculator.java
+++ U2A4P1_Borg_Student Marks/src/MarkCalculator.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,4 +13,16 @@
  */
 public class MarkCalculator {
     
+    public static double studentAverage(ArrayList<ArrayList<String>> studentData, String firstName, String LastName){
+       double theStudentAverage = 0;
+       int getPosition = studentData.indexOf(LastName);
+       
+       return theStudentAverage;
+    }
+    
+    public static double courseAverage(ArrayList<ArrayList<String>> studentData){
+        double theCourseAverage = 0;
+        
+        return theCourseAverage;
+    }
 }
