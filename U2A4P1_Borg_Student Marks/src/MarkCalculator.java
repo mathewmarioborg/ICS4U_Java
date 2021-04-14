7af8c9747d1298cd6070c5f69996f0cc2372d23b
mat4539 <mborg0020@gmail.com>
2021-04-13 21:33:24 -0400

    Finished Student Average function

--- U2A4P1_Borg_Student Marks/src/MarkCalculator.java
+++ U2A4P1_Borg_Student Marks/src/MarkCalculator.java
@@ -1,34 +1,42 @@
-
 import java.util.ArrayList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Mathew Borg
- */
 public class MarkCalculator {
 
-    public static double studentAverage(ArrayList<ArrayList<String>> studentData, String firstName, String LastName) {
-        double theStudentAverage = 0;
+    public static double studentAverage(ArrayList<ArrayList<String>> studentData, String firstName, String lastName) {
+        double theStudentAverage = 0, firstNameColumn = 0, lastNameColumn = 0, dTestOne, dTestTwo, dTestThree, dTestFour;
+        String sTestOne = "", sTestTwo = "", sTestThree = "", sTestFour = "";
+        //get FirstName Position
         for (int i = 0; i < studentData.size(); i++) {
             for (int j = 0; j < studentData.get(i).size(); j++) {
                 if(studentData.get(i).get(j).equals(firstName)){
-                   System.out.println("i=" + i + " j=" + j); 
-                   System.out.println(studentData.get(i).get(0)); 
-                   System.out.println(studentData.get(i).get(1));
-                   System.out.println(studentData.get(i).get(2));
-                   System.out.println(studentData.get(i).get(3));
-                   System.out.println(studentData.get(i).get(4));
-                   System.out.println(studentData.get(i).get(5));
+                    firstNameColumn += i; //adds i to first name column var
+                }
+            }
+        }
+        //get lastNamePosition
+        for (int i = 0; i < studentData.size(); i++) {
+            for (int j = 0; j < studentData.get(i).size(); j++) {
+                if(studentData.get(i).get(j).equals(lastName)){
+                    lastNameColumn += i; //adds i to first name column var
+                    sTestOne = studentData.get(i).get(2);
+                    sTestTwo = studentData.get(i).get(3);
+                    sTestThree = studentData.get(i).get(4);
+                    sTestFour = studentData.get(i).get(5);
                 }
             }
         }
         
+        if(firstNameColumn == lastNameColumn){
+            dTestOne = Double.parseDouble(sTestOne);
+            dTestTwo = Double.parseDouble(sTestTwo);
+            dTestThree = Double.parseDouble(sTestThree);
+            dTestFour = Double.parseDouble(sTestFour);
+            
+            theStudentAverage += dTestOne + dTestTwo + dTestThree + dTestFour;
+            theStudentAverage /= 4;
+        }else{
+            
+        }
         return theStudentAverage;
     }
 
