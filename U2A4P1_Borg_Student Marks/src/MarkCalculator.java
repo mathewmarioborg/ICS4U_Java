7c65c1a43c9be7e08e55a00d7c62aab314598110
mat4539 <mborg0020@gmail.com>
2021-04-14 22:33:21 -0400

    Fixed double lastname/firstname error

--- U2A4P1_Borg_Student Marks/src/MarkCalculator.java
+++ U2A4P1_Borg_Student Marks/src/MarkCalculator.java
@@ -1,16 +1,17 @@
+
 import java.util.ArrayList;
 
 public class MarkCalculator {
+
     static double studentTestOne, studentTestTwo, studentTestThree, studentTestFour;
 
     public static double studentAverage(ArrayList<ArrayList<Double>> studentTestData, ArrayList<ArrayList<String>> studentNameData, String firstName, String lastName, javax.swing.JTextArea outputAverage) {
-        double theStudentAverage = 0, firstNameColumn = 0, lastNameColumn = 0;
+        double theStudentAverage = 0;
         int sumColumn = 0;
         //get FirstName Position
         for (int i = 0; i < studentNameData.size(); i++) {
             for (int j = 0; j < studentNameData.get(i).size(); j++) {
-                if(studentNameData.get(i).get(j).equals(firstName)){
-                    firstNameColumn += i; //adds i to first name column var
+                if (studentNameData.get(i).get(j).equals(firstName)) {
                     sumColumn += i; //adds i to sum column 
                 }
             }
@@ -18,41 +19,37 @@ public class MarkCalculator {
         //get lastNamePosition
         for (int i = 0; i < studentNameData.size(); i++) {
             for (int j = 0; j < studentNameData.get(i).size(); j++) {
-                if(studentNameData.get(i).get(j).equals(lastName)){
-                    lastNameColumn += i; //adds i to first name column var
+                if (studentNameData.get(i).get(j).equals(lastName)) {
                     sumColumn += i; //adds i to sum column 
                 }
             }
         }
-        
-        if(firstNameColumn == lastNameColumn){   
-            sumColumn /= 2;
 
-            studentTestOne = studentTestData.get(sumColumn).get(0);
-            studentTestTwo = studentTestData.get(sumColumn).get(1);
-            studentTestThree = studentTestData.get(sumColumn).get(2);
-            studentTestFour = studentTestData.get(sumColumn).get(3);
-            
-            theStudentAverage += studentTestOne + studentTestTwo + studentTestThree + studentTestFour;
-            theStudentAverage /= 4;
-        }else{
-            outputAverage.setText("No Student Found");
-        }
+        sumColumn /= 2;
+
+        studentTestOne = studentTestData.get(sumColumn).get(0);
+        studentTestTwo = studentTestData.get(sumColumn).get(1);
+        studentTestThree = studentTestData.get(sumColumn).get(2);
+        studentTestFour = studentTestData.get(sumColumn).get(3);
+
+        theStudentAverage += studentTestOne + studentTestTwo + studentTestThree + studentTestFour;
+        theStudentAverage /= 4;
+
         return theStudentAverage;
     }
 
     public static double courseAverage(ArrayList<ArrayList<Double>> studentTestData) {
         double theCourseAverage = 0;
-        
+
         for (int i = 0; i < studentTestData.size(); i++) {
             for (int j = 0; j < studentTestData.get(i).size(); j++) {
                 theCourseAverage += (studentTestData.get(i).get(j));
             }
         }
         theCourseAverage /= studentTestData.size(); //devides the total by the number of students in the array
-        
+
         theCourseAverage /= 4; //devides total by the 4 tests 
-        
+
         return theCourseAverage;
     }
 }
