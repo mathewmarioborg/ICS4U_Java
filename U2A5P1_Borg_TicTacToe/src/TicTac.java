5ea388e5880cbe333003205edcf9afd434540f47
mat4539 <mborg0020@gmail.com>
2021-04-15 22:47:11 -0400

    Step 1

--- U2A5P1_Borg_TicTacToe/src/TicTac.java
+++ U2A5P1_Borg_TicTacToe/src/TicTac.java
@@ -8,8 +8,17 @@
  *
  * @author Mathew Borg
  */
-public class TicTac {
+import java.awt.*;
+import javax.swing.*;
 
-    public static void main(String[] arguments) {
-    }
+public class TicTac extends JFrame {
+
+    JPanel row1 = new JPanel(); //creats outline box
+    JButton[][] boxes = new JButton[3][3]; // creates nine buttons in a 2D array that is 3 x 3.
+    JButton play = new JButton("Play"); //creats play btn
+    JTextField blank1 = new JTextField(); //creats jtextfeild blank1
+    JTextField blank2 = new JTextField(); //creats jtextfeild blank2
+    JOptionPane win = new JOptionPane("Winner"); //creats joptionpane win
+    ImageIcon back = new ImageIcon("cardback.jpg "); //creats new image icon with cardback.jpg
+
 }
