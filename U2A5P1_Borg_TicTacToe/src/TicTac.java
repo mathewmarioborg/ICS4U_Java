30a8a7f1b73e19a6f56ae2bebaaeed503fe0da16
mat4539 <mborg0020@gmail.com>
2021-04-15 23:02:55 -0400

    Step 2

--- U2A5P1_Borg_TicTacToe/src/TicTac.java
+++ U2A5P1_Borg_TicTacToe/src/TicTac.java
@@ -19,6 +19,40 @@ public class TicTac extends JFrame {
     JTextField blank1 = new JTextField(); //creats jtextfeild blank1
     JTextField blank2 = new JTextField(); //creats jtextfeild blank2
     JOptionPane win = new JOptionPane("Winner"); //creats joptionpane win
-    ImageIcon back = new ImageIcon("cardback.jpg "); //creats new image icon with cardback.jpg
+    ImageIcon back = new ImageIcon("src//images//cardback.jpg "); //creats new image icon with cardback.jpg
+
+    public TicTac() { //method to draw game borard
+        super("Tic Tac Toe"); //creates the outer frame with the title
+        setSize(500, 600); //sets the size of the outer frame.
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //sets the program to quit running when the window is closed
+        FlowLayout layout = new FlowLayout(); //arranges components from left to right, centering components horizontally with a five pixel gap between them.
+        setLayout(layout);
+        int name = 0; //creats int name
+        String newname; //creats string newname
+
+        GridLayout layout1 = new GridLayout(4, 3, 10, 10); //arranges the components in a rectangular grid, where all cells are of equal size.
+        row1.setLayout(layout1);
+        //creates and adds the buttons to the GridLayout's first three rows
+        for (int x = 0; x <= 2; x++) {
+            for (int y = 0; y <= 2; y++) {
+                name = name + 1;
+                newname = Integer.toString(name);
+                boxes[x][y] = new JButton(newname);
+                boxes[x][y].setIcon(back);
+                row1.add(boxes[x][y]); 
+            }
+        }
+        //adds remaining components to the GridLayout.
+        row1.add(blank1);
+        row1.add(play);
+        row1.add(blank2);
+        add(row1); //adds the GridLayout to the FlowLayout.
+
+        setVisible(true); // shows the FlowLayout on the screen
+    }
+
+    public static void main(String[] arguments) {
+        TicTac frame = new TicTac(); // runs the screen layout class
+    }
 
 }
