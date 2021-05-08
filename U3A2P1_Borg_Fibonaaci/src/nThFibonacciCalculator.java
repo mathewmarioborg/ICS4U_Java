78d71e9c6b0b713ec63b030c499e5647a63822f0
mat4539 <mborg0020@gmail.com>
2021-05-07 21:30:15 -0400

    nThFibonacciCalculations.java comments

--- U3A2P1_Borg_Fibonaaci/src/nThFibonacciCalculator.java
+++ U3A2P1_Borg_Fibonaaci/src/nThFibonacciCalculator.java
@@ -10,11 +10,11 @@
  */
 public class nThFibonacciCalculator {
 
-    public static int nThCalculator(int num) {
-        if (num == 0 || num == 1) {
-            return num;
-        } else {
-            return (nThCalculator(num - 1) + nThCalculator(num - 2));
+    public static int nThCalculator(int num) { //creats method for calcualting the value of the nth Fibonacci number
+        if (num == 0 || num == 1) { //if num is qual to 0 or qual to 1
+            return num; //return num
+        } else { //else
+            return (nThCalculator(num - 1) + nThCalculator(num - 2)); //return recursive F(n) = F(n-1) + F(n-2)
         }
     }
 
