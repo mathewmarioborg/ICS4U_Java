c17fe73749e179f02ac65dd64b6fc6d4e9cad277
mat4539 <mborg0020@gmail.com>
2021-05-07 21:02:49 -0400

    added GUI Vars

--- U3A2P1_Borg_Fibonaaci/src/Main.java
+++ U3A2P1_Borg_Fibonaaci/src/Main.java
@@ -26,42 +26,42 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
+        titleMain = new javax.swing.JLabel();
+        titleAuthor = new javax.swing.JLabel();
+        titleFirstNums = new javax.swing.JLabel();
+        titleNums = new javax.swing.JLabel();
+        titleEnterNum = new javax.swing.JLabel();
+        calculateFibonacci = new javax.swing.JButton();
+        inputNum = new javax.swing.JTextField();
+        outputFibonacciNum = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 10, 10));
-        jLabel1.setText("Fibonacci Numbers");
-        jLabel1.setFocusTraversalPolicyProvider(true);
+        titleMain.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        titleMain.setForeground(new java.awt.Color(255, 10, 10));
+        titleMain.setText("Fibonacci Numbers");
+        titleMain.setFocusTraversalPolicyProvider(true);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel2.setText("By: Mathew Borg");
+        titleAuthor.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        titleAuthor.setText("By: Mathew Borg");
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel3.setText("The First Nine Fibonacci Numbers Are Listed As Follows:");
+        titleFirstNums.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        titleFirstNums.setText("The First Nine Fibonacci Numbers Are Listed As Follows:");
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel4.setText("1, 1, 2, 3, 5, 8, 13, 21, 34");
+        titleNums.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        titleNums.setText("1, 1, 2, 3, 5, 8, 13, 21, 34");
 
-        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel5.setText("Enter The Fibonacci Number You Would Like To See");
+        titleEnterNum.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        titleEnterNum.setText("Enter The Fibonacci Number You Would Like To See");
 
-        jButton1.setText("Find Fibonacci Number");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        calculateFibonacci.setText("Find Fibonacci Number");
+        calculateFibonacci.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                calculateFibonacciActionPerformed(evt);
             }
         });
 
-        jTextField2.setEditable(false);
+        outputFibonacciNum.setEditable(false);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -71,60 +71,60 @@ public class Main extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jLabel3))
+                        .addComponent(titleFirstNums))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(148, 148, 148)
-                        .addComponent(jButton1))
+                        .addComponent(calculateFibonacci))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(190, 190, 190)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(inputNum, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel4)
+                        .addComponent(titleNums)
                         .addGap(124, 124, 124))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel5))
+                            .addComponent(outputFibonacciNum, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(titleEnterNum))
                         .addGap(25, 25, 25))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1)
+                        .addComponent(titleMain)
                         .addGap(81, 81, 81))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel2)
+                        .addComponent(titleAuthor)
                         .addGap(141, 141, 141))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
+                .addComponent(titleMain)
                 .addGap(1, 1, 1)
-                .addComponent(jLabel2)
+                .addComponent(titleAuthor)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel3)
+                .addComponent(titleFirstNums)
                 .addGap(1, 1, 1)
-                .addComponent(jLabel4)
+                .addComponent(titleNums)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel5)
+                .addComponent(titleEnterNum)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(inputNum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(11, 11, 11)
-                .addComponent(jButton1)
+                .addComponent(calculateFibonacci)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(outputFibonacciNum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void calculateFibonacciActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_calculateFibonacciActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_calculateFibonacciActionPerformed
 
     /**
      * @param args the command line arguments
@@ -162,13 +162,13 @@ public class Main extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JButton calculateFibonacci;
+    private javax.swing.JTextField inputNum;
+    private javax.swing.JTextField outputFibonacciNum;
+    private javax.swing.JLabel titleAuthor;
+    private javax.swing.JLabel titleEnterNum;
+    private javax.swing.JLabel titleFirstNums;
+    private javax.swing.JLabel titleMain;
+    private javax.swing.JLabel titleNums;
     // End of variables declaration//GEN-END:variables
 }
