ba6bc5dd0fc8ec10c1f3e28fcdddda5ae82e3436
mat4539 <mborg0020@gmail.com>
2021-06-08 12:47:30 -0400

    Finished GUI

--- U3A4P1_Borg_SortingRoutines/src/Main.java
+++ U3A4P1_Borg_SortingRoutines/src/Main.java
@@ -28,84 +28,87 @@ public class Main extends javax.swing.JFrame {
 
         sortType = new javax.swing.ButtonGroup();
         sortOrder = new javax.swing.ButtonGroup();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
-        jRadioButton3 = new javax.swing.JRadioButton();
-        jRadioButton4 = new javax.swing.JRadioButton();
-        jRadioButton5 = new javax.swing.JRadioButton();
-        jRadioButton6 = new javax.swing.JRadioButton();
+        title = new javax.swing.JLabel();
+        author = new javax.swing.JLabel();
+        enterInfo = new javax.swing.JLabel();
+        sortAlgorithumText = new javax.swing.JLabel();
+        sortTypeText = new javax.swing.JLabel();
+        ammountNumSort = new javax.swing.JLabel();
+        btnSortNum = new javax.swing.JButton();
+        originalNumbers = new javax.swing.JLabel();
+        sortedNumbers = new javax.swing.JLabel();
+        btnSelection = new javax.swing.JRadioButton();
+        btnBubble = new javax.swing.JRadioButton();
+        btnInsertion = new javax.swing.JRadioButton();
+        btnQuick = new javax.swing.JRadioButton();
+        btnAscending = new javax.swing.JRadioButton();
+        btnDescending = new javax.swing.JRadioButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        outputOriginalNumbers = new javax.swing.JTextArea();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
+        outputSortedNumbers = new javax.swing.JTextArea();
+        inputNumbersToSort = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(10, 10, 255));
-        jLabel1.setText("Sorting Routines");
+        title.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        title.setForeground(new java.awt.Color(10, 10, 255));
+        title.setText("Sorting Routines");
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel2.setText("By: Mathew Borg");
+        author.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        author.setText("By: Mathew Borg");
 
-        jLabel3.setText("Enter The following information:");
+        enterInfo.setText("Enter The following information:");
 
-        jLabel4.setText("Sorting Algorithm:");
+        sortAlgorithumText.setText("Sorting Algorithm:");
 
-        jLabel5.setText("Sort Order");
+        sortTypeText.setText("Sort Order");
 
-        jLabel6.setText("Amount of Numbers to Sort:");
+        ammountNumSort.setText("Amount of Numbers to Sort:");
 
-        jButton1.setText("Sort Numbers");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnSortNum.setText("Sort Numbers");
+        btnSortNum.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnSortNumActionPerformed(evt);
             }
         });
 
-        jLabel7.setText("Original Numbers");
+        originalNumbers.setText("Original Numbers");
 
-        jLabel8.setText("Sorted Numbers");
+        sortedNumbers.setText("Sorted Numbers");
 
-        sortType.add(jRadioButton1);
-        jRadioButton1.setText("jRadioButton1");
+        sortType.add(btnSelection);
+        btnSelection.setText("Selection");
 
-        sortType.add(jRadioButton2);
-        jRadioButton2.setText("jRadioButton2");
+        sortType.add(btnBubble);
+        btnBubble.setText("Bubble");
 
-        sortType.add(jRadioButton3);
-        jRadioButton3.setText("jRadioButton3");
-        jRadioButton3.addActionListener(new java.awt.event.ActionListener() {
+        sortType.add(btnInsertion);
+        btnInsertion.setText("Insertion");
+        btnInsertion.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButton3ActionPerformed(evt);
+                btnInsertionActionPerformed(evt);
             }
         });
 
-        sortType.add(jRadioButton4);
-        jRadioButton4.setText("jRadioButton4");
+        sortType.add(btnQuick);
+        btnQuick.setText("Quick");
 
-        sortOrder.add(jRadioButton5);
-        jRadioButton5.setText("jRadioButton5");
+        sortOrder.add(btnAscending);
+        btnAscending.setText("Ascending");
 
-        sortOrder.add(jRadioButton6);
-        jRadioButton6.setText("jRadioButton6");
+        sortOrder.add(btnDescending);
+        btnDescending.setText("Desending");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        outputOriginalNumbers.setColumns(20);
+        outputOriginalNumbers.setRows(5);
+        outputOriginalNumbers.setEnabled(false);
+        jScrollPane1.setViewportView(outputOriginalNumbers);
 
-        jTextArea2.setColumns(20);
-        jTextArea2.setRows(5);
-        jScrollPane2.setViewportView(jTextArea2);
+        outputSortedNumbers.setColumns(20);
+        outputSortedNumbers.setRows(5);
+        outputSortedNumbers.setEnabled(false);
+        jScrollPane2.setViewportView(outputSortedNumbers);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -116,66 +119,71 @@ public class Main extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel6)
-                            .addComponent(jRadioButton4)
-                            .addComponent(jRadioButton1)
-                            .addComponent(jRadioButton3)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(ammountNumSort)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(inputNumbersToSort, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(btnQuick)
+                            .addComponent(btnSelection)
+                            .addComponent(btnInsertion)
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel4)
+                                    .addComponent(sortAlgorithumText)
                                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(133, 133, 133)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel5)
-                                    .addComponent(jLabel8)
+                                    .addComponent(sortTypeText)
+                                    .addComponent(sortedNumbers)
                                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jRadioButton6)
-                                    .addComponent(jRadioButton5)
-                                    .addComponent(jButton1)))
-                            .addComponent(jLabel7)
-                            .addComponent(jLabel3)
-                            .addComponent(jRadioButton2)))
+                                    .addComponent(btnAscending)
+                                    .addComponent(btnDescending)
+                                    .addComponent(btnSortNum)))
+                            .addComponent(originalNumbers)
+                            .addComponent(enterInfo)
+                            .addComponent(btnBubble)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(122, 122, 122)
-                        .addComponent(jLabel1))
+                        .addComponent(title))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(148, 148, 148)
-                        .addComponent(jLabel2)))
+                        .addComponent(author)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1)
+                .addComponent(title)
                 .addGap(6, 6, 6)
-                .addComponent(jLabel2)
+                .addComponent(author)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel3)
+                .addComponent(enterInfo)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel5))
+                    .addComponent(sortAlgorithumText)
+                    .addComponent(sortTypeText))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jRadioButton1)
-                    .addComponent(jRadioButton6))
+                    .addComponent(btnSelection)
+                    .addComponent(btnAscending))
                 .addGap(8, 8, 8)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jRadioButton2)
-                    .addComponent(jRadioButton5))
+                    .addComponent(btnBubble)
+                    .addComponent(btnDescending))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jRadioButton3)
+                .addComponent(btnInsertion)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jRadioButton4)
-                    .addComponent(jButton1))
+                    .addComponent(btnQuick)
+                    .addComponent(btnSortNum))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel6)
-                .addGap(9, 9, 9)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
-                    .addComponent(jLabel8))
+                    .addComponent(ammountNumSort)
+                    .addComponent(inputNumbersToSort, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(6, 6, 6)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(originalNumbers)
+                    .addComponent(sortedNumbers))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jScrollPane2)
@@ -186,13 +194,13 @@ public class Main extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnSortNumActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSortNumActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnSortNumActionPerformed
 
-    private void jRadioButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton3ActionPerformed
+    private void btnInsertionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInsertionActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jRadioButton3ActionPerformed
+    }//GEN-LAST:event_btnInsertionActionPerformed
 
     /**
      * @param args the command line arguments
@@ -230,26 +238,27 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
-    private javax.swing.JRadioButton jRadioButton3;
-    private javax.swing.JRadioButton jRadioButton4;
-    private javax.swing.JRadioButton jRadioButton5;
-    private javax.swing.JRadioButton jRadioButton6;
+    private javax.swing.JLabel ammountNumSort;
+    private javax.swing.JLabel author;
+    private javax.swing.JRadioButton btnAscending;
+    private javax.swing.JRadioButton btnBubble;
+    private javax.swing.JRadioButton btnDescending;
+    private javax.swing.JRadioButton btnInsertion;
+    private javax.swing.JRadioButton btnQuick;
+    private javax.swing.JRadioButton btnSelection;
+    private javax.swing.JButton btnSortNum;
+    private javax.swing.JLabel enterInfo;
+    private javax.swing.JTextField inputNumbersToSort;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JLabel originalNumbers;
+    private javax.swing.JTextArea outputOriginalNumbers;
+    private javax.swing.JTextArea outputSortedNumbers;
+    private javax.swing.JLabel sortAlgorithumText;
     private javax.swing.ButtonGroup sortOrder;
     private javax.swing.ButtonGroup sortType;
+    private javax.swing.JLabel sortTypeText;
+    private javax.swing.JLabel sortedNumbers;
+    private javax.swing.JLabel title;
     // End of variables declaration//GEN-END:variables
 }
